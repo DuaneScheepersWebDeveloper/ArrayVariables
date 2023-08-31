dcadaa5792796920db0b67d66f8646ee339b268b
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-08-31 09:45:00 +0200

    Initial commit

---
+++ ArrayVariables/Form1.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        heading1 = New Label()
+        btnEx1 = New Button()
+        btnEx2 = New Button()
+        btnEx3 = New Button()
+        btnEx4 = New Button()
+        btnEx5 = New Button()
+        btnEx6 = New Button()
+        btnEx7 = New Button()
+        btnEx8 = New Button()
+        lbl6 = New Label()
+        lbl1 = New Label()
+        lbl2 = New Label()
+        lbl3 = New Label()
+        lbl4 = New Label()
+        lbl5 = New Label()
+        lbl7 = New Label()
+        lbl8 = New Label()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.ImageScalingSize = New Size(24, 24)
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' heading1
+        ' 
+        heading1.AutoSize = True
+        heading1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        heading1.Location = New Point(23, 18)
+        heading1.Name = "heading1"
+        heading1.Size = New Size(355, 25)
+        heading1.TabIndex = 1
+        heading1.Text = "Coding Exercises using arrays with loops"
+        ' 
+        ' btnEx1
+        ' 
+        btnEx1.Location = New Point(23, 56)
+        btnEx1.Name = "btnEx1"
+        btnEx1.Size = New Size(112, 48)
+        btnEx1.TabIndex = 2
+        btnEx1.Text = "Exercise 1"
+        btnEx1.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx2
+        ' 
+        btnEx2.Location = New Point(23, 110)
+        btnEx2.Name = "btnEx2"
+        btnEx2.Size = New Size(112, 50)
+        btnEx2.TabIndex = 3
+        btnEx2.Text = "Exercise 2"
+        btnEx2.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx3
+        ' 
+        btnEx3.Location = New Point(28, 166)
+        btnEx3.Name = "btnEx3"
+        btnEx3.Size = New Size(107, 46)
+        btnEx3.TabIndex = 4
+        btnEx3.Text = "Exercise 3"
+        btnEx3.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx4
+        ' 
+        btnEx4.Location = New Point(28, 218)
+        btnEx4.Name = "btnEx4"
+        btnEx4.Size = New Size(107, 49)
+        btnEx4.TabIndex = 5
+        btnEx4.Text = "Exercise 4"
+        btnEx4.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx5
+        ' 
+        btnEx5.Location = New Point(28, 273)
+        btnEx5.Name = "btnEx5"
+        btnEx5.Size = New Size(107, 47)
+        btnEx5.TabIndex = 6
+        btnEx5.Text = "Exercise 5"
+        btnEx5.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx6
+        ' 
+        btnEx6.Location = New Point(28, 326)
+        btnEx6.Name = "btnEx6"
+        btnEx6.Size = New Size(107, 47)
+        btnEx6.TabIndex = 7
+        btnEx6.Text = "Exercise 6"
+        btnEx6.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx7
+        ' 
+        btnEx7.Location = New Point(28, 379)
+        btnEx7.Name = "btnEx7"
+        btnEx7.Size = New Size(107, 47)
+        btnEx7.TabIndex = 8
+        btnEx7.Text = "Exercise 7"
+        btnEx7.UseVisualStyleBackColor = True
+        ' 
+        ' btnEx8
+        ' 
+        btnEx8.Location = New Point(23, 432)
+        btnEx8.Name = "btnEx8"
+        btnEx8.Size = New Size(112, 38)
+        btnEx8.TabIndex = 9
+        btnEx8.Text = "Exercise 8"
+        btnEx8.UseVisualStyleBackColor = True
+        ' 
+        ' lbl6
+        ' 
+        lbl6.AutoSize = True
+        lbl6.Location = New Point(150, 337)
+        lbl6.Name = "lbl6"
+        lbl6.Size = New Size(505, 25)
+        lbl6.TabIndex = 10
+        lbl6.Text = "Find the smallest item then output the result in a message box"
+        ' 
+        ' lbl1
+        ' 
+        lbl1.AutoSize = True
+        lbl1.Location = New Point(141, 68)
+        lbl1.Name = "lbl1"
+        lbl1.Size = New Size(510, 25)
+        lbl1.TabIndex = 11
+        lbl1.Text = "OutPut each item in a separate message box one after another."
+        ' 
+        ' lbl2
+        ' 
+        lbl2.AutoSize = True
+        lbl2.Location = New Point(141, 123)
+        lbl2.Name = "lbl2"
+        lbl2.Size = New Size(535, 25)
+        lbl2.TabIndex = 12
+        lbl2.Text = "Output all of the items in the same message box on separate lines"
+        ' 
+        ' lbl3
+        ' 
+        lbl3.AutoSize = True
+        lbl3.Location = New Point(141, 177)
+        lbl3.Name = "lbl3"
+        lbl3.Size = New Size(485, 25)
+        lbl3.TabIndex = 13
+        lbl3.Text = "Add up all the items then output the total in a message box"
+        ' 
+        ' lbl4
+        ' 
+        lbl4.AutoSize = True
+        lbl4.Location = New Point(141, 230)
+        lbl4.Name = "lbl4"
+        lbl4.Size = New Size(526, 25)
+        lbl4.TabIndex = 14
+        lbl4.Text = "Calculate the average of the items the output it in a message box"
+        ' 
+        ' lbl5
+        ' 
+        lbl5.AutoSize = True
+        lbl5.Location = New Point(150, 284)
+        lbl5.Name = "lbl5"
+        lbl5.Size = New Size(618, 25)
+        lbl5.TabIndex = 15
+        lbl5.Text = "Add up to the items greater than 20 then output the result in a message box"
+        ' 
+        ' lbl7
+        ' 
+        lbl7.AutoSize = True
+        lbl7.Location = New Point(150, 390)
+        lbl7.Name = "lbl7"
+        lbl7.Size = New Size(442, 25)
+        lbl7.TabIndex = 16
+        lbl7.Text = "Find the smallest item then output it in a message box"
+        ' 
+        ' lbl8
+        ' 
+        lbl8.AutoSize = True
+        lbl8.Location = New Point(141, 439)
+        lbl8.Name = "lbl8"
+        lbl8.Size = New Size(752, 25)
+        lbl8.TabIndex = 17
+        lbl8.Text = "Replace each item in the array with a new value that is twice as big then output the new values"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(822, 56)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(373, 250)
+        Label1.TabIndex = 18
+        Label1.Text = resources.GetString("Label1.Text")
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlLight
+        ClientSize = New Size(1234, 500)
+        Controls.Add(Label1)
+        Controls.Add(lbl8)
+        Controls.Add(lbl7)
+        Controls.Add(lbl5)
+        Controls.Add(lbl4)
+        Controls.Add(lbl3)
+        Controls.Add(lbl2)
+        Controls.Add(lbl1)
+        Controls.Add(lbl6)
+        Controls.Add(btnEx8)
+        Controls.Add(btnEx7)
+        Controls.Add(btnEx6)
+        Controls.Add(btnEx5)
+        Controls.Add(btnEx4)
+        Controls.Add(btnEx3)
+        Controls.Add(btnEx2)
+        Controls.Add(btnEx1)
+        Controls.Add(heading1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents heading1 As Label
+    Friend WithEvents btnEx1 As Button
+    Friend WithEvents btnEx2 As Button
+    Friend WithEvents btnEx3 As Button
+    Friend WithEvents btnEx4 As Button
+    Friend WithEvents btnEx5 As Button
+    Friend WithEvents btnEx6 As Button
+    Friend WithEvents btnEx7 As Button
+    Friend WithEvents btnEx8 As Button
+    Friend WithEvents lbl6 As Label
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents lbl5 As Label
+    Friend WithEvents lbl7 As Label
+    Friend WithEvents lbl8 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ ArrayVariables/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Dim aiData(4) As String
+    Dim i As Integer
+    Public Sub btnEx1_Click(sender As Object, e As EventArgs) Handles btnEx1.Click
+        aiData(0) = "5"
+        aiData(1) = "7"
+        aiData(2) = "33"
+        aiData(3) = "22"
+        aiData(4) = "8"
+
+        For i = 0 To 4
+            MsgBox(aiData(i))
+        Next
+    End Sub
+
+    Public Sub btnEx2_Click(sender As Object, e As EventArgs) Handles btnEx2.Click
+        Dim itotal As Integer
+        For i = 0 To 4
+            itotal += aiData(i)
+        Next
+        MsgBox("Exercise 3 , Your Total is : " & itotal)
+    End Sub
+End Class
